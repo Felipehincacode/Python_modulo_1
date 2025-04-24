0ff2ea2f055ba897fee62c7ecb383eb51896fe3e
Felipe hincapié Murilo <fotopipets@gmail.com>
2025-04-24 08:22:08 -0500

    Add files via upload
    ejericio 2

---
+++ ejercicio2.py
@@ -0,0 +1,16 @@
+#ejercicio2
+
+#Pide un número al usuario. Di si es positivo, negativo o si es cero
+
+number = input ("ingresar un numero:")
+
+if number > 0:
+    print("es positivo")
+elif number < 0:
+    print("es negativo")
+else:
+    print("es cero")
+
+
+
+
