7e32b4b40eebebba634186486f85886c8ff9511d
Felipe hincapié Murilo <fotopipets@gmail.com>
2025-04-29 10:47:48 -0500

    Update entrenamiento_2.py

--- semana_2/entrenamiento_2.py
+++ semana_2/entrenamiento_2.py
@@ -1,1 +1,26 @@
 
+#verificar nota si aprobó o no
+
+def verificacion(): 
+       
+        
+    while True:
+        try:
+            numero = int(input("Ingrese un número entre 0 y 100 "))
+            
+            if numero> 100 or numero<0:
+                print("por favor un ingrese un número dentro del rango")
+                continue 
+            else:
+                break
+        except ValueError: print("Error: Por favor, ingrese solo números enteros. 🚫")
+
+        if numero >= 70:
+                print(f"Su nota es {numero} por lo tanto aprobó la materia 👍")
+        else:
+                print(f"Su nota es {numero} por lo tanto reprobó la materia 👎")
+            
+
+    
+
+print(verificacion())
